b38b87c1845e7c4789f76a8eefd366ea0c354e5b
nooblette <min9hyuk@gmail.com>
2023-09-12 20:12:32 +0900

    feat : MongoDB 연동 및 도서 조회 api MongoDB 조회 로직 추가

---
+++ src/main/java/com/booklog/book/category/common/MainCategoryEnum.java
@@ -0,0 +1,36 @@
+package com.booklog.book.category.common;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum MainCategoryEnum {
+    GENERAL("0", "총류"),
+    PHILOSOPHY("1", "철학"),
+    THEOLOGY("2", "종교학"),
+    SOCIOLOGY("3", "사회학"),
+    NATURAL_SCIENCE("4", "자연과학"),
+    TECHNOLOGY("5", "기술과학"),
+    ARTS("6", "예술학"),
+    LINGUISTICS("7", "언어"),
+    LITERATURE("8", "문학"),
+    HISTORY("9", "역사");
+
+    private final String category;
+    private final String description;
+    MainCategoryEnum(String category, String description){
+        this.category = category;
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    // category 코드값을 입력으로 받아 적절한 description을 반환
+    public static Optional<String> getDescriptionByCategory(String categoryCode) {
+        return Arrays.stream(MainCategoryEnum.values())
+                .filter(m -> m.category.equals(categoryCode))
+                .map(MainCategoryEnum::getDescription)
+                .findFirst();
+    }
+}
--- src/main/java/com/booklog/book/category/dto/Category.java
+++ src/main/java/com/booklog/book/category/dto/Category.java
@@ -1,9 +1,13 @@
 package com.booklog.book.category.dto;
 
+import com.booklog.book.category.common.MainCategoryEnum;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Getter
@@ -12,4 +16,15 @@ import lombok.Getter;
 public class Category {
     private String value;
     private String label;
+
+    public static List<Category> of(List<String> mainCategories){
+        return mainCategories.stream()
+                .flatMap(category -> MainCategoryEnum.getDescriptionByCategory(category)
+                        .map(description -> Category.builder()
+                                .value(category)
+                                .label(description)
+                                .build())
+                        .stream())
+                .collect(Collectors.toList());
+    }
 }
